084762a49624d2b9d49d9cdcfb144cc0341a872f
monjurctg <monjurpy.06@gmail.com>
2023-05-13 20:54:12 +0600

    update

--- src/components/layout/Mainlayout.jsx
+++ src/components/layout/Mainlayout.jsx
@@ -8,24 +8,26 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import {Ionicons} from "@expo/vector-icons";
-import Modal from 'react-native-modal';
+import { Ionicons } from "@expo/vector-icons";
+import Modal from "react-native-modal";
 import React, { useEffect, useState } from "react";
-import {height, scale, width} from "../../../utils/funtions";
-import {colors} from "../../theme/colors";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { height, scale, width } from "../../../utils/funtions";
+import { colors } from "../../theme/colors";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Text from "../tags/Text";
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
+import FullScreenLoader from "../loader/FullScreenLoader ";
 
-const Mainlayout = ({children}) => {
+const Mainlayout = ({ children }) => {
   const [isSearchVisible, setSearchVisible] = useState(false);
-const navigation = useNavigation()
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
   const handleSearchIconClick = () => {
     setSearchVisible(!isSearchVisible);
   };
-  const[isMenu,setIsMenu]=useState(false)
+  const [isMenu, setIsMenu] = useState(false);
   const animatedValue = new Animated.Value(0);
 
   useEffect(() => {
@@ -44,37 +46,44 @@ const navigation = useNavigation()
     }
   }, [isMenu]);
 
-  
-
   const translateY = animatedValue.interpolate({
     inputRange: [0, 1],
     outputRange: [-300, 0],
   });
 
+  const handleMenu = () => {
+    setIsMenu(!isMenu);
+  };
 
-  const handleMenu = ()=>{
-    setIsMenu(!isMenu)
+  const handleLogout = () => {
+    setLoading(true);
 
+    AsyncStorage.clear()
+      .then((abc) => {
+        console.log(abc, "abc from logout clear");
+        // setLoading(false);
+        // navigation.navigate("login")
+        setTimeout(() => {
+          DevSettings.reload();
+        }, 3000);
+      })
+      .catch((err) => {
+        console.log(err, "err from logout");
+        // setLoading(false);
+      })
+      
+      
+  };
+
+  if (loading) {
+    return <FullScreenLoader visible={loading} />;
   }
 
-  const  handleLogout = ()=>{
-
-     AsyncStorage.clear().then(()=>{
-    DevSettings.reload()
-    // navigation.navigate("login")
-
-
-     })
-    
-
-
-  }
-  
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <View>
-          <View style={{paddingHorizontal: scale(10)}}>
+          <View style={{ paddingHorizontal: scale(10) }}>
             <View style={styles.headerContainer}>
               <View style={styles.left}>
                 <TouchableOpacity onPress={handleMenu}>
@@ -89,14 +98,15 @@ const navigation = useNavigation()
                   />
                 </TouchableOpacity>
 
-                <View style={{marginLeft: scale(10)}}>
+                <View style={{ marginLeft: scale(10) }}>
                   <Text
                     preset={["fw_400"]}
                     style={{
                       marginBottom: scale(5),
                       fontSize: 14,
                       lineHeight: 16,
-                    }}>
+                    }}
+                  >
                     Monjurul alam
                   </Text>
                   <TouchableOpacity style={styles.checkBlncBtn}>
@@ -106,7 +116,8 @@ const navigation = useNavigation()
                         color: colors.white,
                         fontSize: 11,
                         lineHeight: 12,
-                      }}>
+                      }}
+                    >
                       Check Balance
                     </Text>
                   </TouchableOpacity>
@@ -116,80 +127,98 @@ const navigation = useNavigation()
                 {/* <TouchableOpacity>
                 <Feather name="search" size={24} color="black" />
                 </TouchableOpacity> */}
-                 <TouchableOpacity onPress={handleSearchIconClick}>
-        <Feather name="search" size={24} color="black" />
-      </TouchableOpacity>
-      
-               
-                <TouchableOpacity >
-      <Feather name="bell" size={24} color="black" />
-      
-        <View style={styles.notificationBadge}>
-          <Text style={styles.notificationText}>2</Text>
-        </View>
-    
-    </TouchableOpacity>
-                 
-    <TouchableOpacity  onPress={() => navigation?.navigate("cart")} style={{marginRight: scale(10)}} >
-    <Feather name="shopping-cart" size={24} color="black" />
-      
-        <View style={styles.notificationBadge}>
-          <Text style={styles.notificationText}>2</Text>
-        </View>
-    
-    </TouchableOpacity>
+                <TouchableOpacity onPress={handleSearchIconClick}>
+                  <Feather name="search" size={24} color="black" />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                  <Feather name="bell" size={24} color="black" />
+
+                  <View style={styles.notificationBadge}>
+                    <Text style={styles.notificationText}>2</Text>
+                  </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={() => navigation?.navigate("cart")}
+                  style={{ marginRight: scale(10) }}
+                >
+                  <Feather name="shopping-cart" size={24} color="black" />
+
+                  <View style={styles.notificationBadge}>
+                    <Text style={styles.notificationText}>2</Text>
+                  </View>
+                </TouchableOpacity>
                 {/* <TouchableOpacity
                   onPress={() => navigation?.navigate("cart")}
                   style={{marginRight: scale(10)}}>
                  
                   <Feather name="shopping-cart" size={24} color="black" />
                 </TouchableOpacity> */}
-
-                
-                
               </View>
             </View>
             {isMenu && (
-        <Animated.View
-          style={[
-            styles.menuContainer,
-            {
-              transform: [{ translateY }],
-              opacity: animatedValue,
-            },
-          ]}
-        >
-          {/* Add your menu items here */}
-          <TouchableOpacity style={styles.menuItem}>
-            <Ionicons name="person-outline" size={24} color={colors.primary_1} style={styles.menuIcon} />
-            {/* Add your profile menu content here */}
-            <Text style={[styles.logoutText,{color:colors.primary_1}]}>Profile</Text>
-            
-          </TouchableOpacity>
+              <Animated.View
+                style={[
+                  styles.menuContainer,
+                  {
+                    transform: [{ translateY }],
+                    opacity: animatedValue,
+                  },
+                ]}
+              >
+                {/* Add your menu items here */}
+                <TouchableOpacity style={styles.menuItem}>
+                  <Ionicons
+                    name="person-outline"
+                    size={24}
+                    color={colors.primary_1}
+                    style={styles.menuIcon}
+                  />
+                  {/* Add your profile menu content here */}
+                  <Text
+                    style={[styles.logoutText, { color: colors.primary_1 }]}
+                  >
+                    Profile
+                  </Text>
+                </TouchableOpacity>
 
-          <TouchableOpacity style={styles.menuItem}>
-            <Ionicons name="settings-outline" size={24} color={colors.primary_1}style={styles.menuIcon} />
-            {/* Add your settings menu content here */}
-            <Text style={[styles.logoutText,{color:colors.primary_1}]}>Settings</Text>
+                <TouchableOpacity style={styles.menuItem}>
+                  <Ionicons
+                    name="settings-outline"
+                    size={24}
+                    color={colors.primary_1}
+                    style={styles.menuIcon}
+                  />
+                  {/* Add your settings menu content here */}
+                  <Text
+                    style={[styles.logoutText, { color: colors.primary_1 }]}
+                  >
+                    Settings
+                  </Text>
+                </TouchableOpacity>
 
-          </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={handleLogout}
+                  style={styles.logoutButton}
+                >
+                  <Ionicons
+                    name="log-out-outline"
+                    size={24}
+                    color="#FFF"
+                    style={styles.menuIcon}
+                  />
+                  <Text style={styles.logoutText}>Logout</Text>
+                </TouchableOpacity>
+              </Animated.View>
+            )}
 
-          <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
-            <Ionicons name="log-out-outline" size={24} color="#FFF" style={styles.menuIcon} />
-            <Text style={styles.logoutText}>Logout</Text>
-          </TouchableOpacity>
-        </Animated.View>
-      )}
-            
-            
             {children}
 
             {/* <View style={{height: scale(300)}}></View> */}
           </View>
         </View>
       </View>
-
-      
     </SafeAreaView>
   );
 };
@@ -240,38 +269,39 @@ const styles = StyleSheet.create({
     // backgroundColor: "blue",
   },
   notificationBadge: {
-    position: 'absolute',
+    position: "absolute",
     top: -8,
     right: -7,
     backgroundColor: colors.primary_2,
     width: 20,
     height: 20,
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   notificationText: {
-    color: 'white',
+    color: "white",
     fontSize: 12,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   searchField: {
     flex: 1,
     marginLeft: 10,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 5,
     paddingVertical: 5,
     paddingHorizontal: 10,
-  }, menuContainer: {
-    position: 'absolute',
-    backgroundColor: '#d1c5c5cf',
+  },
+  menuContainer: {
+    position: "absolute",
+    backgroundColor: "#d1c5c5cf",
     top: 70,
     left: 10,
     minHeight: 160,
-    minWidth:200,
+    minWidth: 200,
     zIndex: 999999,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -281,28 +311,28 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   menuItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderBottomWidth: 1,
-    borderBottomColor: '#be202e',
+    borderBottomColor: "#be202e",
   },
   menuIcon: {
     marginRight: 10,
   },
   logoutButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     // justifyContent: 'center',
-    backgroundColor: '#be202e',
+    backgroundColor: "#be202e",
     paddingVertical: 10,
-    paddingHorizontal:20,
+    paddingHorizontal: 20,
     marginTop: 10,
   },
   logoutText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 14,
-    fontWeight: '600',
+    fontWeight: "600",
   },
 });

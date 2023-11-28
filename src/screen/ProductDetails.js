9d57c5df6c9fb697e17c7ba5dd983e9cb49a2324
monjurctg <monjurpy.06@gmail.com>
2023-11-28 10:59:31 +0600

    update

--- src/screen/ProductDetails.js
+++ src/screen/ProductDetails.js
@@ -159,6 +159,7 @@ const ProductDetails = ({ navigation }) => {
       .then((res) => {
         // console.log(res.data.data.thumbnail_image,"singleproduct")
         // setBigImg(res.data.data.thumbnail_image);
+        console.log(res.data.data)
         setBigImg(res.data.data?.photos[0].path ?? res.data.data.thumbnail_image)
         setLoading(false);
         // console.log(res.data.data?.variant_info)
